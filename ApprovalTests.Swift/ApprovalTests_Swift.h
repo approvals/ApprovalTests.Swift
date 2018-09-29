6e9e8e0294b701e222a7712b587dcac705e61bff
Matthew Carlson <matthew@industriallogic.com>
2018-09-29 14:19:54 -0700

    Initial Commit

---
+++ ApprovalTests.Swift/ApprovalTests_Swift.h
@@ -0,0 +1,19 @@
+//
+//  ApprovalTests_Swift.h
+//  ApprovalTests.Swift
+//
+//  Created by Industrial Logic on 9/29/18.
+//  Copyright © 2018 NA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ApprovalTests_Swift.
+FOUNDATION_EXPORT double ApprovalTests_SwiftVersionNumber;
+
+//! Project version string for ApprovalTests_Swift.
+FOUNDATION_EXPORT const unsigned char ApprovalTests_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApprovalTests_Swift/PublicHeader.h>
+
+
