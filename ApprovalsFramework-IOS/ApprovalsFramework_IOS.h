3f73c3d0096f32ec3da4da9d1382765e3a10d578
Matthew Carlson <matthew@industriallogic.com>
2018-10-02 17:45:09 -0700

    add mutlitple targest to the project

---
+++ ApprovalsFramework-IOS/ApprovalsFramework_IOS.h
@@ -0,0 +1,19 @@
+//
+//  ApprovalsFramework_IOS.h
+//  ApprovalsFramework-IOS
+//
+//  Created by Industrial Logic on 10/2/18.
+//  Copyright © 2018 NA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApprovalsFramework_IOS.
+FOUNDATION_EXPORT double ApprovalsFramework_IOSVersionNumber;
+
+//! Project version string for ApprovalsFramework_IOS.
+FOUNDATION_EXPORT const unsigned char ApprovalsFramework_IOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApprovalsFramework_IOS/PublicHeader.h>
+
+
---
+++ ApprovalsFramework-OSX/ApprovalsFramework_OSX.h
@@ -0,0 +1,19 @@
+//
+//  ApprovalsFramework_OSX.h
+//  ApprovalsFramework-OSX
+//
+//  Created by Industrial Logic on 10/2/18.
+//  Copyright © 2018 NA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ApprovalsFramework_OSX.
+FOUNDATION_EXPORT double ApprovalsFramework_OSXVersionNumber;
+
+//! Project version string for ApprovalsFramework_OSX.
+FOUNDATION_EXPORT const unsigned char ApprovalsFramework_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApprovalsFramework_OSX/PublicHeader.h>
+
+
