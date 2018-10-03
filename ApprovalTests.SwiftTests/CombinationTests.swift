3083921e14b903a6b71ac6205df41ebad2844dc0
Matthew Carlson <matthew@industriallogic.com>
2018-10-02 18:45:03 -0700

    Combinations with 6 params

--- ApprovalTests.SwiftTests/CombinationTests.swift
+++ ApprovalTests.SwiftTests/CombinationTests.swift
@@ -73,4 +73,13 @@ class CombinationTests: XCTestCase {
         try! CombinationApprovals.verifyAllCombinations(inCall, [1,2,3], [4,5], [6,7], [8,9], [10,11])
     }
 
+    func testCombinationsSixParameters() {
+        let inCall: (Int, Int, Int, Int, Int, Int) -> String = {
+            let sum = $0 + $1 + $2 + $3 + $4 + $5
+            return String(sum)
+        }
+        
+        try! CombinationApprovals.verifyAllCombinations(inCall, [1,2,3], [4,5], [6,7], [8,9], [10,11], [12,13])
+    }
+
 }
