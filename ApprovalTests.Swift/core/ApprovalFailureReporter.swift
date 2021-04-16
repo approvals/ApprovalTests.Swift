38035fe0c7847f02e6a1dfb3abb60c3ada08d7ab
Jon Reid <jon@qualitycoding.org>
2021-04-15 21:07:25 -0700

    Revert "R Declare EquatableFailureReporter as open"
    This reverts commit 139ffc3c

--- ApprovalTests.Swift/core/ApprovalFailureReporter.swift
+++ ApprovalTests.Swift/core/ApprovalFailureReporter.swift
@@ -4,16 +4,16 @@ public protocol ApprovalFailureReporter {
     func report(received: String, approved: String) -> Bool
 }
 
-open class EquatableFailureReporter: ApprovalFailureReporter, Equatable {
-    public func report(received: String, approved: String) -> Bool {
+class EquatableFailureReporter: ApprovalFailureReporter, Equatable {
+    func report(received: String, approved: String) -> Bool {
         false
-    }
-
-    public static func ==(lhs: EquatableFailureReporter, rhs: EquatableFailureReporter) -> Bool {
-        lhs.isEqualTo(rhs)
     }
 
     func isEqualTo(_ other: ApprovalFailureReporter) -> Bool {
         false
+    }
+    
+    static func ==(lhs: EquatableFailureReporter, rhs: EquatableFailureReporter) -> Bool {
+        lhs.isEqualTo(rhs)
     }
 }
