5e1c102d8da12acba920ebbb454e320efed2e7dc
Jon Reid <jon@qualitycoding.org>
2021-04-15 20:23:11 -0700

    R Simplify framework header

--- ApprovalTests.Swift/ApprovalTests_Swift.h
+++ ApprovalTests.Swift/ApprovalTests_Swift.h
@@ -1,16 +1,4 @@
-#include <TargetConditionals.h>
-#if TARGET_OS_IOS
-#import <UIKit/UIKit.h>
-#else
-#import <Cocoa/Cocoa.h>
-#endif
+#import <Foundation/Foundation.h>
 
-//! Project version number for ApprovalTests_Swift.
 FOUNDATION_EXPORT double ApprovalTests_SwiftVersionNumber;
-
-//! Project version string for ApprovalTests_Swift.
 FOUNDATION_EXPORT const unsigned char ApprovalTests_SwiftVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ApprovalTests_Swift/PublicHeader.h>
-
-
