771e344dc4c84654c59f94eecec572e00592e70f
Jon Reid <jon@qualitycoding.org>
2021-04-15 21:20:49 -0700

    a Reformat

--- ApprovalTests.Swift/Utils/StringUtils.swift
+++ ApprovalTests.Swift/Utils/StringUtils.swift
@@ -1,6 +1,5 @@
 enum StringUtils {
 
-    /***********************************************************************/
     public static func padNumber(_ number: Int, _ digits: Int) -> String {
         var text: String = "" + String(number)
         while (text.count < digits) {
@@ -9,17 +8,13 @@ enum StringUtils {
         return text
     }
 
-    /***********************************************************************/
     public static func toString(_ name: String, _ array: [Any]) -> String {
         var buffer: String = ""
-        if (array.isEmpty)
-        {
+        if (array.isEmpty) {
             buffer.append(name + ".length = 0");
-        }
-        else
-        {
+        } else {
             let maxPadding: Int = (String(array.count).count)
-            for index in 0..<array.count {
+            for index in 0 ..< array.count {
                 let padding = padNumber(index, maxPadding)
                 buffer.append(name + "[" + padding + "] = " + String(describing: array[index]) + "\n")
             }
