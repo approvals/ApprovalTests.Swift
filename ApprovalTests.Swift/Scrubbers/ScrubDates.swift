a249049190f7bc20347b5132fc0b4464dccaf5f0
Jon Reid <jon@qualitycoding.org>
2021-04-29 17:16:33 -0700

    R Extracted ScrubWithRegExWithReplacementAliasing
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- ApprovalTests.Swift/Scrubbers/ScrubDates.swift
+++ ApprovalTests.Swift/Scrubbers/ScrubDates.swift
@@ -1,14 +1,22 @@
 import Foundation
 
-public class ScrubDates: ScrubWithRegEx {
-    public init() {
+public class ScrubWithRegExWithReplacementAliasing: ScrubWithRegEx {
+    public init(pattern: String, aliaser: @escaping (Int) -> String) {
         var hash: [String: Int] = [:]
-        super.init(pattern: "\\d{4}-\\d{1,2}-\\d{1,2}T\\d{1,2}:\\d{2}:\\d{2}Z", replacementFunction: { d in
+        super.init(pattern: pattern, replacementFunction: { d in
             var index = hash.count + 1
             if hash[d] == nil {
                 hash[d] = index
             }
             index = hash[d]!
+            return aliaser(index)
+        })
+    }
+}
+
+public class ScrubDates: ScrubWithRegExWithReplacementAliasing {
+    public init() {
+        super.init(pattern: "\\d{4}-\\d{1,2}-\\d{1,2}T\\d{1,2}:\\d{2}:\\d{2}Z", aliaser: { index in
             return "<date\(index)>"
         })
     }
