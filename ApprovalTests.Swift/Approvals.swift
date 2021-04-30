686dcf0b59e240b70a5c3b6c6cbee95ebb562123
Jon Reid <jon@qualitycoding.org>
2021-04-29 22:08:49 -0700

    R Tighten lines in Approvals.swift

--- ApprovalTests.Swift/Approvals.swift
+++ ApprovalTests.Swift/Approvals.swift
@@ -11,48 +11,41 @@ public enum Approvals {
         NameCreator().load(file.description)
     }
 
-    public static func verifyAsJSON<T: Encodable>(
-            _ object: T,
-            _ options: Options = Options(),
-            file: StaticString = #filePath,
-            line: UInt = #line
-    ) throws {
+    public static func verifyAsJSON<T: Encodable>(_ object: T,
+                                                  _ options: Options = Options(),
+                                                  file: StaticString = #filePath,
+                                                  line: UInt = #line) throws {
         try verify(StringUtils.toJSON(object), options.forFile.withExtension(".json"), file: file, line: line)
     }
 
-    public static func verifyAll(
-            _ label: String,
-            _ array: [Any],
-            _ options: Options = Options(),
-            file: StaticString = #filePath,
-            line: UInt = #line
-    ) throws {
+    public static func verifyAll(_ label: String,
+                                 _ array: [Any],
+                                 _ options: Options = Options(),
+                                 file: StaticString = #filePath,
+                                 line: UInt = #line) throws {
         try verify(StringUtils.toString(label, array), options, file: file, line: line)
     }
 
-    public static func verify(
-            _ response: String,
-            _ options: Options = Options(),
-            file: StaticString = #filePath,
-            line: UInt = #line
+    public static func verify(_ response: String,
+                              _ options: Options = Options(),
+                              file: StaticString = #filePath,
+                              line: UInt = #line
     ) throws {
         try verify(ApprovalTextWriter(options.scrub(response), options.forFile.fileExtensionWithoutDot), options, file: file, line: line)
     }
 
-    private static func verify(
-            _ writer: ApprovalTextWriter,
-            _ options: Options = Options(),
-            file: StaticString,
-            line: UInt) throws {
+    private static func verify(_ writer: ApprovalTextWriter,
+                               _ options: Options = Options(),
+                               file: StaticString,
+                               line: UInt) throws {
         try verify(writer, createApprovalNamer(file.description), options, file: file, line: line)
     }
 
-    private static func verify(
-            _ writer: ApprovalTextWriter,
-            _ namer: ApprovalNamer,
-            _ options: Options = Options(),
-            file: StaticString,
-            line: UInt) throws {
+    private static func verify(_ writer: ApprovalTextWriter,
+                               _ namer: ApprovalNamer,
+                               _ options: Options = Options(),
+                               file: StaticString,
+                               line: UInt) throws {
         try verify(FileApprover(writer, namer), options, file: file, line: line)
     }
 
@@ -69,12 +62,10 @@ public enum Approvals {
         }
     }
 
-    public static func verify<T>(
-            _ object: T,
-            _ options: Options = Options(),
-            file: StaticString = #filePath,
-            line: UInt = #line
-    ) throws {
+    public static func verify<T>(_ object: T,
+                                 _ options: Options = Options(),
+                                 file: StaticString = #filePath,
+                                 line: UInt = #line) throws {
         let description = String(describing: type(of: object.self)) + String(describing: object)
         try verify(description, options, file: file, line: line)
     }
@@ -82,7 +73,7 @@ public enum Approvals {
     public static func createApprovalNamer(_ file: String) -> ApprovalNamer {
         NameCreator().load(file)
     }
-    
+
     public static var reporter: ApprovalFailureReporter {
         ReporterFactory.get
     }
