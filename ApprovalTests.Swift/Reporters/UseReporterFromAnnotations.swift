40c3495741704809ccbd67878714b464c1a7629c
Jon Reid <jon@qualitycoding.org>
2021-05-02 15:18:56 -0700

    R Delete unused UseReporterFromAnnotations

--- ApprovalTests.Swift/Reporters/UseReporterFromAnnotations.swift
+++
@@ -1,8 +0,0 @@
-public class UseReporterFromAnnotations: ApprovalFailureReporter {
-    public init() {}
-
-    public func report(received: String, approved: String) -> Bool {
-        ReporterFactory.get
-                       .report(received: received, approved: approved)
-    }
-}
