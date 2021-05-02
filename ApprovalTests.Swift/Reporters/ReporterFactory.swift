8f87233f34966ec002c641e7127ccbbe81d9b045
Jon Reid <jon@qualitycoding.org>
2021-05-02 15:17:20 -0700

    R Remove unused FRONTLOADED_REPORTER and USE_REPORTER

--- ApprovalTests.Swift/Reporters/ReporterFactory.swift
+++ ApprovalTests.Swift/Reporters/ReporterFactory.swift
@@ -1,7 +1,4 @@
 class ReporterFactory {
-    let FRONTLOADED_REPORTER = "FrontloadedReporter"
-    let USE_REPORTER = "UseReporter"
-
     public static var get: ApprovalFailureReporter {
         #if os(OSX)
             let returned = MacReporter()
