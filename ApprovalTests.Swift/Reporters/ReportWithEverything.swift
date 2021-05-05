065a1e1918924ed9bbe93eaa04aa57499c6455fe
Jon Reid <jon@qualitycoding.org>
2021-05-04 17:26:18 -0700

    F!! Report with everything (multi reporter)
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

---
+++ ApprovalTests.Swift/Reporters/ReportWithEverything.swift
@@ -0,0 +1,20 @@
+class ReportWithEverything: EquatableFailureReporter {
+    private let reporters: [EquatableFailureReporter]
+
+    public init(_ reporters: EquatableFailureReporter...) {
+        self.reporters = reporters
+    }
+
+    override func report(received: String, approved: String) -> Bool {
+        var worked = false
+        for reporter in reporters {
+            worked = reporter.report(received: received, approved: approved) || worked
+        }
+        return worked
+    }
+
+    override func isEqualTo(_ other: ApprovalFailureReporter) -> Bool {
+        guard let other = other as? ReportWithEverything else { return false }
+        return reporters == other.reporters
+    }
+}
--- ApprovalTests.SwiftTests/Reporters/ReporterTests.swift
+++ ApprovalTests.SwiftTests/Reporters/ReporterTests.swift
@@ -19,4 +19,17 @@ final class ReporterTests: XCTestCase {
         XCTAssertEqual(reporter2.approved, "a.text")
         XCTAssertEqual(reporter3.approved, "")
     }
+
+    func testMultiReporter() throws {
+        let reporter1 = TestReporter(success: true)
+        let reporter2 = TestReporter(success: false)
+        let reporter3 = TestReporter(success: true)
+
+        let success = ReportWithEverything(reporter1, reporter2, reporter3).report(received: "r.text", approved: "a.text")
+
+        XCTAssertTrue(success)
+        XCTAssertEqual(reporter1.approved, "a.text")
+        XCTAssertEqual(reporter2.approved, "a.text")
+        XCTAssertEqual(reporter3.approved, "a.text")
+    }
 }
