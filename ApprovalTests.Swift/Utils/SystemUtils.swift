8c137c7fb31ace20bcb51b372499d42f5f335ee7
Jon Reid <jon@qualitycoding.org>
2021-05-04 18:03:29 -0700

    R Simplify platform conditionals
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- ApprovalTests.Swift/Utils/SystemUtils.swift
+++ ApprovalTests.Swift/Utils/SystemUtils.swift
@@ -1,17 +1,19 @@
 #if os(iOS)
     import UIKit
-#else
-    import AppKit
-#endif
 
-enum SystemUtils {
-    static func pasteToClipboard(_ text: String) {
-        #if os(iOS)
+    enum SystemUtils {
+        static func pasteToClipboard(_ text: String) {
             let pasteboard = UIPasteboard.general
             pasteboard.string = text
-        #else
+        }
+    }
+#else
+    import AppKit
+
+    enum SystemUtils {
+        static func pasteToClipboard(_ text: String) {
             let pasteboard = NSPasteboard.general
             pasteboard.setString(text, forType: .string)
-        #endif
+        }
     }
-}
+#endif
