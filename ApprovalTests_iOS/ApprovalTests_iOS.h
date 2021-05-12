3be7a225d483eb3c0171849042973b82f8c42cf2
Jon Reid <jon@qualitycoding.org>
2021-05-11 21:43:17 -0700

    e Remove cruft from iOS framework header

--- ApprovalTests_iOS/ApprovalTests_iOS.h
+++ ApprovalTests_iOS/ApprovalTests_iOS.h
@@ -1,12 +1,4 @@
-#import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for ApprovalTests_iOS.
 FOUNDATION_EXPORT double ApprovalTests_iOSVersionNumber;
-
-//! Project version string for ApprovalTests_iOS.
 FOUNDATION_EXPORT const unsigned char ApprovalTests_iOSVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ApprovalTests_iOS/PublicHeader.h>
-
-
