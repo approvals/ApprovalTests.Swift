62806a7e01dc2adce267fd83de866a25dfa58420
Jon Reid <jon@qualitycoding.org>
2021-05-20 17:33:13 -0700

    F!! Common reporters
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- ApprovalTests.Swift/Reporters/DiffMergeReporter.swift
+++ ApprovalTests.Swift/Reporters/DiffMergeReporter.swift
@@ -5,3 +5,46 @@ class DiffMergeReporter: GenericDiffReporter {
         })
     }
 }
+
+class ReportWithBeyondCompare: GenericDiffReporter {
+    init() {
+        super.init(programPath: "/Applications/Beyond Compare.app/Contents/MacOS/bcomp")
+    }
+}
+
+// Requires Tools->Create Command-line Launcher
+class ReportWithAppCode: GenericDiffReporter {
+    init() {
+        super.init(programPath: "/usr/local/bin/appcode", arguments: { received, approved in
+            ["diff", received, approved]
+        })
+    }
+}
+
+class ReportWithKdiff3: GenericDiffReporter {
+    init() {
+        super.init(programPath: "/Applications/kdiff3.app/Contents/MacOS/kdiff3", arguments: { received, approved in
+            [received, approved, "-m"]
+        })
+    }
+}
+
+class ReportWithP4Merge: GenericDiffReporter {
+    init() {
+        super.init(programPath: "/Applications/p4merge.app/Contents/MacOS/p4merge")
+    }
+}
+
+class ReportWithTKDiff: GenericDiffReporter {
+    init() {
+        super.init(programPath: "/Applications/TkDiff.app/Contents/MacOS/tkdiff")
+    }
+}
+
+class ReportWithVisualStudioCode: GenericDiffReporter {
+    init() {
+        super.init(programPath: "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code", arguments: { received, approved in
+            ["-d", received, approved]
+        })
+    }
+}
--- ApprovalTests.Swift/Reporters/macOS/MacReporter.swift
+++ ApprovalTests.Swift/Reporters/macOS/MacReporter.swift
@@ -3,8 +3,14 @@ class MacReporter: FirstWorkingReporter {
         #if os(macOS)
             super.init(
                     // begin-snippet: mac_reporters
+                    ReportWithBeyondCompare(),
+                    ReportWithAppCode(),
                     DiffMergeReporter(),
-                    KaleidoscopeReporter()
+                    KaleidoscopeReporter(),
+                    ReportWithP4Merge(),
+                    ReportWithKdiff3(),
+                    ReportWithTKDiff(),
+                    ReportWithVisualStudioCode()
                     // end-snippet
             )
         #endif
