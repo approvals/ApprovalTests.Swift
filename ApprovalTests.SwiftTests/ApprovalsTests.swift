d02ac33dfc295d5634d2d9038a6c8255c24b2d9a
Jon Reid <jon@qualitycoding.org>
2021-06-15 20:00:24 -0700

    R Move GameOfLife down, and private

--- ApprovalTests.SwiftTests/ApprovalsTests.swift
+++ ApprovalTests.SwiftTests/ApprovalsTests.swift
@@ -5,37 +5,6 @@
 #endif
 import XCTest
 
-class GameOfLife: CustomStringConvertible {
-    private var board: (Int, Int) -> Bool
-
-    init(_ board: @escaping (Int, Int) -> Bool) {
-        self.board = board
-    }
-
-    func advance() -> GameOfLife {
-        return GameOfLife({ (x, y) in
-            let c = [
-                self.board(x - 1, y - 1),
-                self.board(x - 0, y - 1),
-                self.board(x + 1, y - 1),
-                self.board(x - 1, y),
-//                self.board(x - 0, y),
-                self.board(x + 1, y),
-                self.board(x - 1, y + 1),
-                self.board(x - 0, y + 1),
-                self.board(x + 1, y + 1),
-            ].filter({ b in b }).count
-            return c == 3 || (c == 2 && self.board(x, y))
-        })
-    }
-
-    var description: String {
-        return StringUtils.printGrid(5, 5, { (x: Int, y: Int) in
-            return board(x, y) ? "X " : ". "
-        })
-    }
-}
-
 final class ApprovalsTests: XCTestCase {
 
     func testToString() throws {
@@ -67,4 +36,35 @@ private struct MyRect: Codable {
     let y: Int
     let width: Int
     let height: Int
+}
+
+private class GameOfLife: CustomStringConvertible {
+    private var board: (Int, Int) -> Bool
+
+    init(_ board: @escaping (Int, Int) -> Bool) {
+        self.board = board
+    }
+
+    func advance() -> GameOfLife {
+        return GameOfLife({ (x, y) in
+            let c = [
+                self.board(x - 1, y - 1),
+                self.board(x - 0, y - 1),
+                self.board(x + 1, y - 1),
+                self.board(x - 1, y),
+//                self.board(x - 0, y),
+                self.board(x + 1, y),
+                self.board(x - 1, y + 1),
+                self.board(x - 0, y + 1),
+                self.board(x + 1, y + 1),
+            ].filter({ b in b }).count
+            return c == 3 || (c == 2 && self.board(x, y))
+        })
+    }
+
+    var description: String {
+        return StringUtils.printGrid(5, 5, { (x: Int, y: Int) in
+            return board(x, y) ? "X " : ". "
+        })
+    }
 }
