c379db2dbfa2010a08d32883133778067097a6d6
Jon Reid <jon@qualitycoding.org>
2021-07-07 11:27:06 -0700

    e Update runtests.sh and exit when any command fails

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-swift build -v 
-swift test -v
+set -e # exit when any command fails
+
+swift build -v
+xcodebuild test -project ApprovalTests.Swift.xcodeproj -scheme ApprovalTests.Swift 
 xcodebuild test -project ApprovalTests.Swift.xcodeproj -scheme ApprovalTests_iOS -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 8"
