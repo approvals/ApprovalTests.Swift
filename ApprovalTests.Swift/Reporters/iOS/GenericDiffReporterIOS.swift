38d35063985cf9cd2b6c8849dc7320150b2ca0ea
Jon Reid <jon@qualitycoding.org>
2021-12-23 16:26:28 -0800

    B Fix iOS by removing call to cleanUpFileNames

--- ApprovalTests.Swift/Reporters/iOS/GenericDiffReporterIOS.swift
+++ ApprovalTests.Swift/Reporters/iOS/GenericDiffReporterIOS.swift
@@ -3,19 +3,17 @@
     import Foundation
     public class GenericDiffReporter: GenericDiffReporterBase {
         public override func runProcess(received: String, approved: String) throws {
-            let (workingReceived, workingApproved) = cleanUpFileNames(received: received, approved: approved)
-
-            let arguments2 = arguments(workingReceived, workingApproved)
+            let arguments2 = arguments(received, approved)
             let process1 = "\(programPath) \(arguments2.joined(separator: " "))"
 
             let process = "#!/bin/bash\n\(process1)"
-            var fileUrl = URL(fileURLWithPath: workingReceived)
+            var fileUrl = URL(fileURLWithPath: received)
             fileUrl.deleteLastPathComponent()
             fileUrl = fileUrl.appendingPathComponent("command.sh")
             do {
                 try process.write(toFile: fileUrl.path, atomically: true, encoding: .utf8)
             } catch {
-                print("An error occurred writing file:" + workingReceived)
+                print("An error occurred writing file:" + received)
                 print("Error: \(error)")
             }
         }
