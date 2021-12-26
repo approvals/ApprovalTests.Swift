0bb7342a27646ffe879a9e44a9409c874ef5f01f
Jon Reid <jon@qualitycoding.org>
2021-12-26 14:54:30 -0800

    R Delete unused Loader/Saver protocols

--- ApprovalTests.Swift/Utils/Persistence.swift
+++ ApprovalTests.Swift/Utils/Persistence.swift
@@ -1,15 +1,5 @@
 import Foundation
 
-public protocol Loader {
-    associatedtype T
-    func load() -> T
-}
-
-public protocol Saver {
-    associatedtype T
-    func save(_ instanceToSave: T) -> T
-}
-
 public protocol ExecutableQuery {
     func getQuery() -> String
     func executeQuery(_ query: String) -> String
