af2729299e7fda5e3546d1fb1a72bc403d52c223
Jon Reid <jon@qualitycoding.org>
2021-12-27 16:49:42 -0800

    R Inline doc for ReportContentsWithXCTest

--- ApprovalTests.Swift/Reporters/ReportContentsWithXCTest.swift
+++ ApprovalTests.Swift/Reporters/ReportContentsWithXCTest.swift
@@ -1,5 +1,12 @@
 import XCTest
 
+/**
+ A reporter that outputs straight to XCTest.
+ 
+ In addition to reporting the change using a standard XCTest assertion, this reporter also creates a
+ command line `mv` command for approving the last failed test. It writes this command to the console
+ and also places it on the clipboard.
+ */
 public class ReportContentsWithXCTest: EquatableFailureReporter {
 
     public override func isEqualTo(_ other: ApprovalFailureReporter) -> Bool {
