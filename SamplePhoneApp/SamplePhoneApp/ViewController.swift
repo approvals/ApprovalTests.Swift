4cd1a8b34d1a1b3fd286a006bd174d74fa906c6a
Jon Reid <jon@qualitycoding.org>
2022-01-04 15:53:47 -0800

    F!! SamplePhoneApp reads max, chooses random number
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- SamplePhoneApp/SamplePhoneApp/ViewController.swift
+++ SamplePhoneApp/SamplePhoneApp/ViewController.swift
@@ -1,9 +1,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private(set) var maxInputTextField: UITextField!
     @IBOutlet private(set) var rollOutputsView: RollOutputsView!
 
     @IBAction private func roll() {
-        rollOutputsView.setRoll(4)
+        rollOutputsView.setRoll(getRandomNumber(max: getMax()))
+    }
+
+    private func getMax() -> Int {
+        let defaultDie = 6
+        return Int(maxInputTextField.text!) ?? defaultDie
+    }
+
+    private func getRandomNumber(max: Int) -> Int {
+        Int.random(in: 1...max)
     }
 }
