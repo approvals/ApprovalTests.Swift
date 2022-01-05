ea7a18813f6f7b05cc939cae1c6d1ab823ddca80
Jon Reid <jon@qualitycoding.org>
2022-01-04 22:16:05 -0800

    F!! Begin expanding iOSApprovalsWatcher to watch all dirs

--- ApprovalTests_SwiftTests/iOSApprovalsWatcher.py
+++ ApprovalTests_SwiftTests/iOSApprovalsWatcher.py
@@ -3,6 +3,7 @@ import datetime
 import os
 import stat
 import subprocess
+import sys
 
 def monitor_file(file_name):
     date = datetime.datetime(2000,1,1)
@@ -15,5 +16,22 @@ def monitor_file(file_name):
                 os.chmod(file_name, os.stat(file_name).st_mode | stat.S_IEXEC) 
                 subprocess.Popen("./" + file_name)
 
+def watchlist(relative_path):
+    absolute_path = os.path.abspath(relative_path)
+    directories = directories_in(absolute_path)
+    date = datetime.datetime(2000,1,1)
+    return [(os.path.join(dir, 'command.sh'), date) for dir in directories]
+
+def directories_in(root):
+    paths = []
+    for file in os.listdir(root):
+        path = os.path.join(root, file)
+        if os.path.isdir(path):
+            paths.append(path)
+            directories_in(path)
+    return paths
+
 if __name__ == "__main__":
-    monitor_file("command.sh")
+    relative_path = sys.argv[1]
+    print(watchlist(relative_path))
+#    monitor_file("command.sh")
