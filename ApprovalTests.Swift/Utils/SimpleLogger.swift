2b99be5e50d2b0693d0b644a045bf513103caa37
Jon Reid <jon@qualitycoding.org>
2022-05-10 16:37:10 -0700

    F!! SimpleLogger with indentation and variables
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- ApprovalTests.Swift/Utils/SimpleLogger.swift
+++ ApprovalTests.Swift/Utils/SimpleLogger.swift
@@ -8,19 +8,55 @@ public class OutMarker {
     }
 
     deinit {
-        print("<- out: " + text)
+        SimpleLogger.indent -= 1
+        SimpleLogger.printer(SimpleLogger.getIndentation() +  "<- out: " + text)
+    }
+}
+
+public class StringBuffer: CustomStringConvertible {
+    public var description: String {
+        "\n" + string
+    }
+
+    var string: String = ""
+    
+    public func append(_ s: String) {
+        string += s
     }
 }
 
 public enum SimpleLogger {
+    static var indent = 0
+    
+    public static func logToString() -> StringBuffer {
+        let output = StringBuffer()
+        Self.printer = { s in 
+            output.append(s + "\n")
+        }
+        return output
+    }
+
+    public static func variable<T>(_ label: String, _ value: T) {
+        Self.printer(getIndentation() + "variable: \(label) = \(String(describing: value))")
+    }
+
+    static var printer: (String) -> Void = { w in
+        print(w)
+    }
+    
     public static func useMarkers(_ function: String = #function, file: StaticString = #file, line _: UInt = #line) -> OutMarker {
         let text = function + getFileInfo(file)
-        print("-> in: " + text)
+        printer(getIndentation() + "-> in: " + text)
+        indent += 1
         return OutMarker(text)
     }
 
+    public static func getIndentation() -> String {
+        String(repeating: " ", count: indent * 2)
+    }
+
     public static func printLineNumber(_: String = #function, file: StaticString = #file, line: UInt = #line) {
-        print("## " + getFileInfo(file) + ":\(line)")
+        printer(getIndentation() + "## " + getFileInfo(file) + ":\(line)")
     }
 
     private static func getFileInfo(_ file: StaticString) -> String {
---
+++ ApprovalTests_SwiftTests/Utils/SimpleLoggerTests.swift
@@ -0,0 +1,46 @@
+#if os(OSX)
+    @testable import ApprovalTests_Swift
+#elseif os(iOS)
+    @testable import ApprovalTests_iOS
+#endif
+import XCTest
+
+final class SimpleLoggerTests: XCTestCase {
+    func test() throws {
+        let output = SimpleLogger.logToString()
+        do {
+            let m = SimpleLogger.useMarkers()
+            SimpleLogger.printLineNumber()
+            do {
+                let m2 = SimpleLogger.useMarkers()
+                let name = "llewellyn"
+                SimpleLogger.variable("name", name);
+            }
+        }
+        try Approvals.verify(output)
+    }
+}
+
+/*
+ public void test()
+ {
+     StringBuffer output = SimpleLogger.logToString();
+     try (Markers m = SimpleLogger.useMarkers();)
+     {
+         try (Markers m2 = SimpleLogger.useMarkers();)
+         {
+             SimpleLogger.event("Starting Logging");
+             String name = "llewellyn";
+             SimpleLogger.variable("name", name);
+             SimpleLogger.query("Select * from people");
+             for (int i = 0; i < 42; i++)
+             {
+                 SimpleLogger.hourGlass();
+             }
+             SimpleLogger.variable("Numbers", new Integer[]{1, 2, 3, 4, 5});
+             SimpleLogger.warning(new Error());
+         }
+     }
+     Approvals.verify(output);
+ }
+ */
