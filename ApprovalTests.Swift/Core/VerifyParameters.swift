a627b82a46a4beca25d7bb1a87895797f647a5c0
Jon Reid <jon@qualitycoding.org>
2022-07-05 20:09:41 -0700

    r - Make VerifyParameters options public

--- ApprovalTests.Swift/Core/VerifyParameters.swift
+++ ApprovalTests.Swift/Core/VerifyParameters.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public class VerifyParameters {
-    private(set) var options: Options
+    public private(set) var options: Options
 
     public init(_ options: Options) { self.options = options }
 }
