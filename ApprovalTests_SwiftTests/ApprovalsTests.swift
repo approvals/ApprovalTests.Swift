becbc8307b5057a7a822f3dc561151e2a86d265d
Tsungyu Yu <ytyubox@gmail.com>
2023-02-09 19:55:01 +0800

    fix linux test

--- ApprovalTests_SwiftTests/ApprovalsTests.swift
+++ ApprovalTests_SwiftTests/ApprovalsTests.swift
@@ -4,11 +4,14 @@
   import ApprovalTests_Swift
 #endif
 import XCTest
+import Foundation
 
 final class ApprovalsTests: XCTestCase {
-    func testToString() throws {
-        try Approvals.verify(CGRect(x: 5, y: 10, width: 100, height: 200))
-    }
+    #if os(macOS)
+        func testToString() throws {
+            try Approvals.verify(CGRect(x: 5, y: 10, width: 100, height: 200))
+        }
+    #endif
 
     func testAsJson() throws {
         try Approvals.verifyAsJSON(CGRect(x: 5, y: 10, width: 100, height: 200))
