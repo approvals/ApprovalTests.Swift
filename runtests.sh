89bc10d70785becb659989f2dd7e9ae901c3f90b
Jon Reid <jon@qualitycoding.org>
2024-05-07 16:14:04 -0700

    e - use iPhone 15 simulator for tests

--- runtests.sh
+++ runtests.sh
@@ -4,4 +4,4 @@ set -e # exit when any command fails
 
 swift build -v
 xcodebuild test -project ApprovalTests.Swift.xcodeproj -scheme ApprovalTests.Swift 
-xcodebuild test -project ApprovalTests.Swift.xcodeproj -scheme ApprovalTests_iOS -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 8"
+xcodebuild test -project ApprovalTests.Swift.xcodeproj -scheme ApprovalTests_iOS -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 15"
