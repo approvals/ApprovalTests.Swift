c5f928ad3d1fca184ee3704d7001801dff3505f6
Jon Reid <jon@qualitycoding.org>
2024-07-03 15:02:05 -0700

    . e Turn off code signing for iOS tests for faster feedback

--- run_tests_ios.sh
+++ run_tests_ios.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-xcodebuild test -project ApprovalTests.Swift.xcodeproj -scheme ApprovalTests_iOS -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 15"
+SCHEME='ApprovalTests_iOS'
+DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 15'
+
+xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO'
